b1cc0b095aca7c12f7f75771adc98c1091f6291e
Ho Ngoc Quyen <quyenhn@wolffungame.com>
2023-04-13 15:06:35 +0700

    chores: setup project

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  Text,
+  useColorScheme,
+} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Text>Setup</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+export default App;
