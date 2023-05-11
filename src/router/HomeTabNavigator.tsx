3a433ee5a8fc233040ed5f5410e977b50387ee38
Ho Ngoc Quyen <quyenhn@wolffungame.com>
2023-05-11 17:23:33 +0700

    refactor: change name folder and refactor router navigation logic

---
+++ src/router/HomeTabNavigator.tsx
@@ -0,0 +1,53 @@
+import {
+  BottomTabBarProps,
+  createBottomTabNavigator,
+} from '@react-navigation/bottom-tabs';
+import CustomTabBar from 'components/CustomTabBar/CustomTabBar';
+import React from 'react';
+import EventPage from 'src/screens/eventPage/EventPage';
+import HomePage from 'src/screens/home/HomePage';
+
+const Tab = createBottomTabNavigator();
+const icShop = require('../../assets/img/ic_shop.png');
+const icEvent = require('../../assets/img/ic_event.png');
+const icQuest = require('../../assets/img/ic_quest.png');
+const icGame = require('../../assets/img/ic_game.png');
+const icMenu = require('../../assets/img/ic_menu.png');
+
+const HomeTabNavigator = () => {
+  const BottomTab = React.useCallback(
+    (props: BottomTabBarProps) => <CustomTabBar {...props} />,
+    [],
+  );
+  return (
+    <Tab.Navigator screenOptions={{headerShown: false}} tabBar={BottomTab}>
+      <Tab.Screen
+        name="Market"
+        component={HomePage}
+        options={{tabBarLabel: 'Market', tabBarIcon: icShop}}
+      />
+      <Tab.Screen
+        name="Event"
+        component={EventPage}
+        options={{tabBarLabel: 'Event', tabBarIcon: icEvent}}
+      />
+      <Tab.Screen
+        name="Daily Quest"
+        component={EventPage}
+        options={{tabBarLabel: 'Quest', tabBarIcon: icQuest}}
+      />
+      <Tab.Screen
+        name="Game"
+        component={EventPage}
+        options={{tabBarLabel: 'Game', tabBarIcon: icGame}}
+      />
+      <Tab.Screen
+        name="Menu"
+        component={EventPage}
+        options={{tabBarLabel: 'Menu', tabBarIcon: icMenu}}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default HomeTabNavigator;
